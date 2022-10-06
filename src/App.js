d48ef52ee4eab7e52b6214e2003236bf2f564bad
Mr.Dominic <ochu.dominic.onah@gmail.com>
2022-10-06 01:10:41 +0100

    text:added the title and import review comp

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import React from "react";
+import Reviews from "./Reviews";
+
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>Our Reviews</h2>
+          <div className="underline"></div>
+        </div>
+        <Reviews />
+      </section>
+    </main>
+  );
 }
 
 export default App;
